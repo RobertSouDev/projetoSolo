e1c0afecbc3f29d677ea9b31c497b77318c482bb
RobertSouDev <rogerhallks1@gmail.com>
2024-02-16 20:44:51 -0300

    posicao do texto do main

---
+++ src/components/Button/ButtonMain.jsx
@@ -0,0 +1,13 @@
+import "./ButtonMain.css"
+ 
+ function ButtonMain(){
+    return(
+        <>
+            <button className="btn">
+                 Ver Ofertas
+            </button>
+        </>
+    )
+ }
+
+ export default ButtonMain
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,43 @@
-
+import ButtonMain from "../Button/ButtonMain"
+import "./Main.css"
+import main from "../../assets/main.png"
 
 export default function Main(){
     return (
         <>
             <div className="main">
-                <div className="mainFilha"></div>
+
+                <div className="mainFilha1">
+                    <div >
+                        <div className="neto">
+                            <p>
+                                <span>Melhores ofertas personalizadas</span>
+                            </p>
+                        </div>
+
+                        <div className="neto">
+                            <h1>
+                                <strong>
+                                    Queima  de<br />
+                                    stoque Nike
+                                </strong>
+                            </h1>
+                        </div>
+
+                        <div className="neto">
+                            <p>
+                                Consequat culpa exercitation mollit nisi excepteur do <br />
+                                do tempor laboris eiusmod irure consectetur
+                            </p>
+                        </div>
+
+                        <div >
+                            <ButtonMain/>
+                        </div>
+                    </div>
+
+         
+                </div>
             </div>
         </>
     )
